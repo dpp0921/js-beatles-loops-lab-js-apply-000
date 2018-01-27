e5edf4b796a8dbeb2a509364ae08c12c80d7c832
Chen Ye <dpp0921@gmail.com>
2018-01-27 15:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  const array = []
   for(var i = 0; i < musicians.length; i++){
-    return `${musicians[i]} plays ${instruments}`
+    array.push`${musicians[i]} plays ${instruments}`
   }
 }
 function johnLennonFacts(facts){
