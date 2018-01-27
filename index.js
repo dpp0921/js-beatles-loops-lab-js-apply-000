8648557fd940930b855028a45b519895a840d341
Chen Ye <dpp0921@gmail.com>
2018-01-27 16:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 function johnLennonFacts(facts){
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
   var i = 0;
   while( i < facts.length ){
     facts = facts + "!!!";
